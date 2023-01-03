61d81a336186df0d3aed787d8439b72957f92d3a
Lukas Olsen <lukmarwil@gmail.com>
2023-01-03 17:01:56 +0100

    Another Extension.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import nltk
 import re
 
 print("Downloading required libraries from NLTK.")
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+nltk.download('stopwords', quiet=True)
+nltk.download('wordnet', quiet=True)
+nltk.download('omw-1.4', quiet=True)
 print("Downloaded all the required libraries.")
 
 from nltk.corpus import stopwords
@@ -31,12 +31,228 @@ import imaplib, email
 
 import os, sys, json
 
+try:
+    import tkinter
+    import customtkinter
+    from PIL import Image
+
+except:
+    print("Failed to import Tkinter or Customtkinter. View docs")
+    exit(0)
+
 #Original Source: https://www.youtube.com/watch?v=dVKNy4eA3tI
+
+class GUI(object):
+    def __init__(self):
+        customtkinter.set_appearance_mode("light")  # Modes: "System" (standard), "Dark", "Light"
+        customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+        
+        self.main = Main()
+
+        self.app = customtkinter.CTk()
+        self.app.geometry("400x780")
+        self.app.title("NettVett - Spam Radar")
+        p1 = tkinter.PhotoImage(file = 'input/GUI/icon_192x192.png')
+        self.app.iconphoto(False, p1)
+
+        self.password_icon = customtkinter.CTkImage(Image.open(os.path.join("input/GUI/", "password.png")), size=(26, 26))
+        self.username_icon = customtkinter.CTkImage(Image.open(os.path.join("input/GUI/", "user.png")), size=(26, 26))
+        
+        self.frame_1 = customtkinter.CTkFrame(master=self.app)
+        self.user_frame = customtkinter.CTkFrame(master=self.frame_1, bg_color="transparent")
+        self.password_frame = customtkinter.CTkFrame(master=self.frame_1, bg_color="transparent")
+
+        self.password = customtkinter.CTkEntry(master=self.password_frame,
+                               placeholder_text="App-Password",
+                               placeholder_text_color="#000000",
+                               #text_font="none 10",
+                               text_color="black",
+                               font=("Helvetica", 14),
+                               
+                               width=250,
+                               height=35,
+                               border_width=2,
+                               border_color= "#aba9a9",
+                               bg_color="#e6e1e1",
+                               fg_color= "#e6e1e1",
+                               
+                               corner_radius=5)
+
+        self.user = customtkinter.CTkEntry(master=self.user_frame,
+                               placeholder_text="Email.",
+                               placeholder_text_color="#000000",
+                               #text_font="none 10",
+                               text_color="black",
+                               font=("Helvetica", 14),
+                               
+                               width=250,
+                               height=35,
+                               border_width=2,
+                               border_color= "#aba9a9",
+                               bg_color="#e6e1e1",
+                               fg_color= "#e6e1e1",
+                               
+                               corner_radius=5)
+
+    def button_callback():
+        print("Button click", combobox_1.get())
+
+
+    def slider_callback(self, value):
+        progressbar_1.set(value)
+
+
+    def work(self):
+
+        
+        self.frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+
+        label_1 = customtkinter.CTkLabel(master=self.frame_1, text="Nettvett | Spam-Radar", font=("none", 36), text_color="#85BF41",justify=tkinter.LEFT)
+        label_1.pack(pady=10, padx=10)
+
+        
+        self.user_frame.pack(pady=20, padx=35)
+
+        
+
+        
+
+        user_image = customtkinter.CTkLabel(master=self.user_frame, text="",image=self.username_icon)
+        user_image.pack(side="right")
+        self.user.pack(side="left", padx=10, pady=10)
+
+        
+        self.password_frame.pack(pady=0, padx=35)
+
+        
+
+        
+
+        password_image = customtkinter.CTkLabel(master=self.password_frame, text="",image=self.password_icon)
+        password_image.pack(side="right")
+        self.password.pack(side="left", padx=10, pady=10)
+
+        self.login_status = customtkinter.CTkLabel(master=self.frame_1, text="Not connected.", text_color="gray")
+        self.login_status.pack(padx=10, pady=0)
+
+        login_btn = customtkinter.CTkButton(
+        master = self.frame_1,
+        text= "Login",
+        command=self.login,
+        #text_font="none 12 bold",
+        text_color="white",
+        hover= True,
+        hover_color= "#81b867",
+        height=40,
+        width= 120,
+        border_width=2,
+        corner_radius=5,
+        border_color= "#608a4d", 
+        bg_color="#262626",
+        fg_color= "#79ae61")
+
+        login_btn.pack(pady=10, padx=10)
+
+
+        read_frame = customtkinter.CTkFrame(self.app)
+        read_frame.pack(padx=10, pady=100)
+        
+        read_emails_btn = customtkinter.CTkButton(
+        master = read_frame,
+        text= "Find Spam",
+        command=self.read,
+        #text_font="none 12 bold",
+        text_color="white",
+        hover= True,
+        hover_color= "#81b867",
+        height=40,
+        width= 120,
+        border_width=2,
+        corner_radius=5,
+        border_color= "#608a4d", 
+        bg_color="#262626",
+        fg_color= "#79ae61"
+        )
+
+        read_emails_btn.pack(pady=100, padx=10)
+
+
+
+        
+        # progressbar_1 = customtkinter.CTkProgressBar(master=self.frame_1)
+        # progressbar_1.pack(pady=10, padx=10)
+
+        # button_1 = customtkinter.CTkButton(master=self.frame_1, command=self.button_callback)
+        # button_1.pack(pady=10, padx=10)
+
+        # slider_1 = customtkinter.CTkSlider(master=self.frame_1, command=self.slider_callback, from_=0, to=1)
+        # slider_1.pack(pady=10, padx=10)
+        # slider_1.set(0.5)
+
+        # entry_1 = customtkinter.CTkEntry(master=self.frame_1, placeholder_text="CTkEntry")
+        # entry_1.pack(pady=10, padx=10)
+
+        # optionmenu_1 = customtkinter.CTkOptionMenu(self.frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
+        # optionmenu_1.pack(pady=10, padx=10)
+        # optionmenu_1.set("CTkOptionMenu")
+
+        # combobox_1 = customtkinter.CTkComboBox(self.frame_1, values=["Option 1", "Option 2", "Option 42 long long long..."])
+        # combobox_1.pack(pady=10, padx=10)
+        # optionmenu_1.set("CTkComboBox")
+
+        # checkbox_1 = customtkinter.CTkCheckBox(master=self.frame_1)
+        # checkbox_1.pack(pady=10, padx=10)
+
+        # radiobutton_var = tkinter.IntVar(value=1)
+
+        # radiobutton_1 = customtkinter.CTkRadioButton(master=self.frame_1, variable=radiobutton_var, value=1)
+        # radiobutton_1.pack(pady=10, padx=10)
+
+        # radiobutton_2 = customtkinter.CTkRadioButton(master=self.frame_1, variable=radiobutton_var, value=2)
+        # radiobutton_2.pack(pady=10, padx=10)
+
+        # switch_1 = customtkinter.CTkSwitch(master=self.frame_1)
+        # switch_1.pack(pady=10, padx=10)
+
+        # text_1 = customtkinter.CTkTextbox(master=self.frame_1, width=200, height=70)
+        # text_1.pack(pady=10, padx=10)
+        # text_1.insert("0.0", "CTkTextbox\n\n\n\n")
+
+        # segmented_button_1 = customtkinter.CTkSegmentedButton(master=self.frame_1, values=["CTkSegmentedButton", "Value 2"])
+        # segmented_button_1.pack(pady=10, padx=10)
+
+        # tabview_1 = customtkinter.CTkTabview(master=self.frame_1, width=200, height=70)
+        # tabview_1.pack(pady=10, padx=10)
+        # tabview_1.add("CTkTabview")
+        # tabview_1.add("Tab 2")
+
+        self.app.mainloop()
+
+    def read(self):
+        print("yea")
+
+    def login(self):
+        print("Accessed function login..")
+        if self.main.login(self.user.get(), self.password.get()):
+            self.login_status.configure(text="Connection secured.", text_color="green")
+        else:
+            self.login_status.configure(text="Connection failed.", text_color="red")
 
 class Main(object):
     def __init__(self):
         self.root = os.path.abspath(os.curdir)
         self.config = self.root + "/config/conf.json"
+
+        self.mail = imaplib.IMAP4_SSL("imap.gmail.com")
+
+    def login(self, username, password):
+        try:
+            rv, message = self.mail.login(username, password)
+            return True
+        except:
+            return False
+        #print(self.mail.login(username, password))
+
 
     def read_emails(self, user, passw):
         print("Trying to connect to imap.gmail.com")
@@ -45,15 +261,15 @@ class Main(object):
         rv, message = mb.login(user, passw)
         print("Logging into the account.")
         # 'OK', [b'LOGIN completed']
-        rv, num_emails = M.select('Inbox')
+        rv, num_emails = mb.select('Inbox')
         # 'OK', [b'22']
 
         # Get unread messages
-        rv, messages = M.search(None, 'UNSEEN')
+        rv, messages = mb.search(None, 'UNSEEN')
         # 'OK', [b'21 22']
 
         # Download a message
-        typ, data = M.fetch(b'21', '(RFC822)')
+        typ, data = mb.fetch(b'21', '(RFC822)')
 
         # Parse the email
         msg = email.message_from_bytes(data[0][1])
@@ -112,12 +328,12 @@ class Main(object):
                 prediction_on_training_data = model.predict(X_train_features)
                 accuracy_on_training_data = accuracy_score(Y_train, prediction_on_training_data)
 
-                print(title + "Accuracy on the training data : ", accuracy_on_training_data)
+                print('- ' + title + " Accuracy on the training data : ", accuracy_on_training_data)
 
                 prediction_on_test_data = model.predict(X_test_features)
                 accuracy_on_test_data = accuracy_score(Y_test, prediction_on_test_data)
 
-                print(title + "Accuracy on test data : ", accuracy_on_test_data)
+                print("- " + title + " Accuracy on test data : ", accuracy_on_test_data)
 
                 #Prediction on email
 
@@ -137,10 +353,10 @@ class Main(object):
 
             if spam == True:
                 self.stats(1, 0)
-                print("We marked the email as spam.")
+                print("We marked the email as spam.\n")
             else:
                 self.stats(0, 1)
-                print("We did not find anything weird.")
+                print("We did not find anything weird.\n")
 
     def advanced_analysis(self, content):
         path = 'input/data/hamnspam/'
@@ -298,4 +514,8 @@ class Main(object):
                     self.linkStorage(match)
 main = Main()
 
-main.worker()
+#main.worker()
+#main.read_emails("lukeproductions3@gmail.com", "mmwyktfkyqzauyif")
+Main().login("lukproductions3@gmail.com", 'asdk')
+
+GUI().work()
